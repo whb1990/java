6e7332d3e1c621225f8024c5094635adb4cd1e99
wanghongbo <270028806@qq.com>
2019-08-26 17:16:09 +0800

    测试线程的Join方法，增加部分注释内容；

--- src/main/java/com/study/thread/ThreadJoinTest.java
+++ src/main/java/com/study/thread/ThreadJoinTest.java
@@ -4,6 +4,7 @@ package main.java.com.study.thread;
  * @author: whb
  * @date: 2019/8/26 17:10
  * @description: A线程需要用到B线程，C线程，D线程的结果
+ * Thread的join()方法的原理：如果线程thread调用join()方法，那么它会阻塞当前正在运行的线程，记着是阻塞当前正在运行的线程，而不是阻塞thread线程，直到thread线程运行结束，才会继续往下执行。
  */
 public class ThreadJoinTest {
 
@@ -25,15 +26,20 @@ public class ThreadJoinTest {
     private static int count3;
 
     public static void main(String[] args) throws InterruptedException {
+        //主线程运行，启动3个线程
         Thread t1 = new Thread(() -> count1 = 10);
         Thread t2 = new Thread(() -> count2 = 20);
         Thread t3 = new Thread(() -> count3 = 30);
         t1.start();
         t2.start();
         t3.start();
+        //主线程会被阻塞，直到线程t1运行结束
         t1.join();
+        //主线程会被阻塞，直到线程t2运行结束
         t2.join();
+        //主线程会被阻塞，直到线程t3运行结束
         t3.join();
+        //主线程运行
         totalCount = count1 + count2 + count3;
         System.out.println("totalCount = " + totalCount);
     }
