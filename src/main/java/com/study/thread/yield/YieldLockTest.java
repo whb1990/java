ae7d80cc53815234cb59362e357cb1ed5f332789
wanghongbo <270028806@qq.com>
2019-09-02 14:23:50 +0800

    线程让步示例；

---
+++ src/main/java/com/study/thread/yield/YieldLockTest.java
@@ -0,0 +1,40 @@
+package main.java.com.study.thread.yield;
+
+/**
+ * @author: whb
+ * @date: 2019/9/2 14:15
+ * @description: wait()的作用是让当前线程由“运行状态”进入“等待(阻塞)状态”的同时，也会释放同步锁。而yield()的作用是让步，它也会让当前线程离开“运行状态”。它们的区别是：
+ * (01) wait()是让线程由“运行状态”进入到“等待(阻塞)状态”，而不yield()是让线程由“运行状态”进入到“就绪状态”。
+ * (02) wait()是会线程释放它所持有对象的同步锁，而yield()方法不会释放锁。
+ */
+public class YieldLockTest {
+
+    private static Object obj = new Object();
+
+    public static void main(String[] args) {
+        ThreadA t1 = new ThreadA("t1");
+        ThreadA t2 = new ThreadA("t2");
+        t1.start();
+        t2.start();
+    }
+
+    static class ThreadA extends Thread {
+        public ThreadA(String name) {
+            super(name);
+        }
+
+        @Override
+        public void run() {
+            // 获取obj对象的同步锁
+            synchronized (obj) {
+                for (int i = 0; i < 10; i++) {
+                    System.out.printf("%s [%d]:%d\n", this.getName(), this.getPriority(), i);
+                    // i整除4时，调用yield
+                    if (i % 4 == 0) {
+                        Thread.yield();
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ src/main/java/com/study/thread/yield/YieldTest.java
@@ -0,0 +1,32 @@
+package main.java.com.study.thread.yield;
+
+/**
+ * @author: whb
+ * @date: 2019/9/2 14:12
+ * @description: yield()的作用是让步。它能让当前线程由“运行状态”进入到“就绪状态”，从而让其它具有相同优先级的等待线程获取执行权；但是，并不能保证在当前线程调用yield()之后，其它具有相同优先级的线程就一定能获得执行权；也有可能是当前线程又进入到“运行状态”继续运行！
+ */
+public class YieldTest {
+    public static void main(String[] args) {
+        ThreadA t1 = new ThreadA("t1");
+        ThreadA t2 = new ThreadA("t2");
+        t1.start();
+        t2.start();
+    }
+}
+
+class ThreadA extends Thread {
+    public ThreadA(String name) {
+        super(name);
+    }
+
+    @Override
+    public synchronized void run() {
+        for (int i = 0; i < 10; i++) {
+            System.out.printf("%s [%d]:%d\n", this.getName(), this.getPriority(), i);
+            // i整除4时，调用yield
+            if (i % 4 == 0) {
+                Thread.yield();
+            }
+        }
+    }
+}
