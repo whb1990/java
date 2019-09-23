0c69ec1540bb2b55067271e21aa8e7f82dfc05c7
wanghongbo <270028806@qq.com>
2019-09-23 12:00:15 +0800

    测试Callable+Future

---
+++ src/main/java/com/study/thread/TestCallableFuture.java
@@ -0,0 +1,48 @@
+package main.java.com.study.thread;
+
+import java.util.concurrent.*;
+
+/**
+ * @author: whb
+ * @date: 2019/9/23 11:57
+ * @description: 使用Callable+Future获取执行结果
+ */
+public class TestCallableFuture {
+    public static void main(String[] args) {
+        ExecutorService executor = Executors.newCachedThreadPool();
+        Task task = new Task();
+        Future<Integer> result = executor.submit(task);
+        executor.shutdown();
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e1) {
+            e1.printStackTrace();
+        }
+
+        System.out.println("主线程在执行任务");
+
+        try {
+            System.out.println("task运行结果" + result.get());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("所有任务执行完毕");
+    }
+}
+
+class Task implements Callable<Integer> {
+    @Override
+    public Integer call() throws Exception {
+        System.out.println("子线程在进行计算");
+        Thread.sleep(3000);
+        int sum = 0;
+        for (int i = 0; i < 100; i++) {
+            sum += i;
+        }
+        return sum;
+    }
+}
