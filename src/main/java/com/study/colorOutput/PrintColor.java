d84af8f631df72c4570ee130e5551d70ce38ba17
wanghongbo <270028806@qq.com>
2019-09-27 20:15:41 +0800

    测试控制台内容彩色输出

---
+++ src/main/java/com/study/colorOutput/PrintColor.java
@@ -0,0 +1,27 @@
+package main.java.com.study.colorOutput;
+
+/**
+ * @author: whb
+ * @date: 2019/9/27 20:14
+ * @description: 彩色输出
+ */
+public class PrintColor {
+    public static void main(String[] args) {
+        System.out.println("\033[30;4m" + "我滴个颜什" + "\033[0m");
+        System.out.println("\033[31;4m" + "我滴个颜什" + "\033[0m");
+        System.out.println("\033[32;4m" + "我滴个颜什" + "\033[0m");
+        System.out.println("\033[33;4m" + "我滴个颜什" + "\033[0m");
+        System.out.println("\033[34;4m" + "我滴个颜什" + "\033[0m");
+        System.out.println("\033[35;4m" + "我滴个颜什" + "\033[0m");
+        System.out.println("\033[36;4m" + "我滴个颜什" + "\033[0m");
+        System.out.println("\033[37;4m" + "我滴个颜什" + "\033[0m");
+        System.out.println("\033[40;31;4m" + "我滴个颜什" + "\033[0m");
+        System.out.println("\033[41;32;4m" + "我滴个颜什" + "\033[0m");
+        System.out.println("\033[42;33;4m" + "我滴个颜什" + "\033[0m");
+        System.out.println("\033[43;34;4m" + "我滴个颜什" + "\033[0m");
+        System.out.println("\033[44;35;4m" + "我滴个颜什" + "\033[0m");
+        System.out.println("\033[45;36;4m" + "我滴个颜什" + "\033[0m");
+        System.out.println("\033[46;37;4m" + "我滴个颜什" + "\033[0m");
+        System.out.println("\033[47;4m" + "我滴个颜什" + "\033[0m");
+    }
+}
