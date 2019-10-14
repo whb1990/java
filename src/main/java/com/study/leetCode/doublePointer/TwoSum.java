04603c6e66edb358068c163ed074ba34a764fcee
wanghongbo <270028806@qq.com>
2019-10-14 11:55:53 +0800

    LeetCode-双指针-有序数组的Two Sum

---
+++ src/main/java/com/study/leetCode/doublePointer/TwoSum.java
@@ -0,0 +1,38 @@
+package main.java.com.study.leetCode.doublePointer;
+
+import java.util.Arrays;
+
+/**
+ * @author: whb
+ * @date: 2019/10/14 11:43
+ * @description: 有序数组的Two Sum
+ * 题目描述：在有序数组中找出两个数，使它们的和为 target。
+ * 使用双指针，一个指针指向值较小的元素，一个指针指向值较大的元素。指向较小元素的指针从头向尾遍历，指向较大元素的指针从尾向头遍历。
+ * <p>
+ * 如果两个指针指向元素的和 sum == target，那么得到要求的结果；
+ * 如果 sum > target，移动较大的元素，使 sum 变小一些；
+ * 如果 sum < target，移动较小的元素，使 sum 变大一些。
+ */
+public class TwoSum {
+    public static int[] twoSum(int[] numbers, int target) {
+        int i = 0, j = numbers.length - 1;
+        while (i < j) {
+            int sum = numbers[i] + numbers[j];
+            if (sum == target) {
+                return new int[]{numbers[i], numbers[j]};
+            } else if (sum < target) {
+                i++;
+            } else {
+                j--;
+            }
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        int[] numbers = {2, 3, 4, 7, 11, 13, 15};
+        int target = 9;
+        int[] result = twoSum(numbers, target);
+        System.out.println(Arrays.toString(result));
+    }
+}
