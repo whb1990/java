e7ff3b9eda9ab05365d29e9419b75e938f550139
wanghongbo <270028806@qq.com>
2020-03-05 18:25:28 +0800

    修改快速排序算法；

--- src/main/java/com/study/sortingAalgorithm/basic_sort/QuickSort.java
+++ src/main/java/com/study/sortingAalgorithm/basic_sort/QuickSort.java
@@ -23,7 +23,7 @@ public class QuickSort {
         // 需要注意的是这个if语句不能少，不然没法停止，会导致堆栈溢出的异常。
         if (left < right) {
             //分割数组，找到分割点
-            int point = partitionTwo(numArr, left, right);
+            int point = partitionFour(numArr, left, right);
             //递归调用，对左子数组进行快速排序
             quickSort(numArr, left, point - 1);
             //递归调用，对右子数组进行快速排序
@@ -81,9 +81,103 @@ public class QuickSort {
     }
 
     /**
+     * 最原始的partition写法:
+     * 在[L, R]之间，选取arr[L]为划分点key；
+     * 然后从[L, R]，如果当前arr[i] < key，就放到左边部分swap(arr, i, ++pivot);，否则就不动；
+     * 最后将数组划分成了arr[L...p-1] < arr[p] ， arr[p+1...R] > arr[p]，并返回p；
+     *
+     * @param arr
+     * @param left
+     * @param right
+     * @return
+     */
+    public static int partitionThree(int[] arr, int left, int right) {
+        //直接选取 arr[L]作为pivot(中心点)
+        int key = arr[left];
+        int pivot = left;
+        for (int i = left + 1; i <= right; i++) {
+            if (arr[i] < key) {
+                swap(arr, i, ++pivot);
+            }
+        }
+        // 将arr[L]放到pivot位置(中间) --> 完成了按照arr[L]划分数组的目的
+        swap(arr, pivot, left);
+        return pivot;
+    }
+
+    /**
+     * 双路快速排序-解决重复元素多的问题
+     * 当我们要排序的数组重复元素很多的情况下，还是会使得划分极其的不均匀:
+     * 第一个解决的方法: 换一种划分的方式:
+     * <p>
+     * 将 <key和>key的元素放在数组的两边，更准确的说是: 左端放的是 <=key的元素，右端放的是>=key的元素；
+     * 然后设置两个指针(一个从L开始，一个从R开始)，然后向中间靠拢，分别找到第一个>=key(左边)、<=key(右边)的元素，就停止扫描，然后交换这两个位置，终止条件是两个指针相碰；
+     * 为什么这样就可以解决重复元素多的问题呢? 因为两个指针的元素相等且都等于key的时候，还是要交换两个位置，这样就不会将重复的元素集中在同一侧。
+     *
+     * @param arr
+     * @param L
+     * @param R
+     * @return
+     */
+    public static int partitionFour(int[] arr, int L, int R) {
+        int key = arr[L];
+        int less = L + 1, more = R;
+        while (true) {
+            while (less < R && arr[less] < key) {
+                less++;
+            }
+            while (more > L && arr[more] > key) {
+                more--;
+            }
+            if (less >= more) {
+                break;
+            }
+            swap(arr, less++, more--);
+        }
+        swap(arr, L, more);
+        return more;
+    }
+
+    /**
+     * 三路快速排序--更好的解决重复元素多的问题。三路快排关键在于partion的过程(荷兰国旗问题)，也就是将一个数组按照某个数划分成三部分:
+     * <p>
+     * 先从序列中选取一个数作为基数(key)；
+     * 分区过程，将<key放到左边，>key的放在右边，=key放到中间；
+     * 再对左右区间重复第二步，直到各区间只有一个数；
+     * 返回的p数组中p[0]代表的是等于区域的左边界，p[1]代表的是等于区域的右边界；
+     *
+     * @param arr
+     * @param left
+     * @param right
+     * @param num
+     * @return
+     */
+    public static int[] partitionFive(int[] arr, int left, int right, int num) {
+        //小于部分的最后一个数
+        int less = left - 1;
+        int more = right + 1;
+        int cur = left;
+        while (cur < more) {
+            if (arr[cur] < num) {
+                //把这个比num小的数放到小于区域的下一个，并且把小于区域扩大一个单位
+                swap(arr, ++less, cur++);
+            } else if (arr[cur] > num) {
+                //把这个比num大的数放到大于区域的下一个，并且把大于区域扩大一个单位
+                //同时，因为从大于区域拿过来的数是未知的，所以不能cur++，还要再次判断一次arr[cur]
+                swap(arr, --more, cur);
+            } else {
+                //否则的话就直接移动
+                cur++;
+            }
+        }
+        //返回的是等于区域的两个下标
+        return new int[]{less + 1, more - 1};
+    }
+
+    /**
      * 交换数组中两个位置的元素
      */
-    public static void swap(int[] numArr, int left, int right) {
+    private static void swap(int[] numArr, int left, int right) {
         int temp = 0;
         if (numArr != null && numArr.length > 0) {
             temp = numArr[left];
