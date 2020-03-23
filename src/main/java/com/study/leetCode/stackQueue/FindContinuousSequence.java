915713e6f5389ffa9ec20c287c785da35b12a8a9
wanghongbo <270028806@qq.com>
2020-03-23 11:08:07 +0800

    滑动窗口求解：LeetCode面试题57 - II. 和为s的连续正数序列

--- src/main/java/com/study/leetCode/stackQueue/FindContinuousSequence.java
+++ src/main/java/com/study/leetCode/stackQueue/FindContinuousSequence.java
@@ -1,4 +1,4 @@
-package main.java.com.study.leetCode;
+package main.java.com.study.leetCode.stackQueue;
 
 import java.util.ArrayList;
 import java.util.LinkedList;
@@ -7,7 +7,7 @@ import java.util.List;
 /**
  * @author: whb
  * @date: 2020/3/7 19:39
- * @description: 面试题57 - II. 和为s的连续正数序列
+ * @description: LeetCode面试题57 - II. 和为s的连续正数序列
  * 输入一个正整数 target ，输出所有和为 target 的连续正整数序列（至少含有两个数）。
  * <p>
  * 序列内的数字由小到大排列，不同序列按照首个数字从小到大排列。
@@ -28,40 +28,14 @@ import java.util.List;
  * <p>
  * 1 <= target <= 10^5
  */
-public class leetCode57II {
+public class FindContinuousSequence {
+    /**
+     * 滑动窗口算法
+     *
+     * @param target
+     * @return
+     */
     public static int[][] findContinuousSequence(int target) {
-        List<List<Integer>> result = new ArrayList<>();
-        for (int i = 1; i < target; i++) {
-            Integer sum = 0;
-            List<Integer> numList = new ArrayList<>();
-            for (int j = i; j < target; j++) {
-                sum = sum + j;
-                if (sum < target) {
-                    numList.add(j);
-                } else if (sum == target) {
-                    numList.add(j);
-                    result.add(numList);
-                    break;
-                } else {
-                    numList = new ArrayList<Integer>();
-                    break;
-                }
-            }
-        }
-        int[][] resultArr = new int[result.size()][];
-        List<Integer> tmpList = null;
-        for (int i = 0; i < result.size(); i++) {
-            tmpList = result.get(i);
-            int[] tmpArr = new int[tmpList.size()];
-            for (int j = 0; j < tmpList.size(); j++) {
-                tmpArr[j] = tmpList.get(j);
-            }
-            resultArr[i] = tmpArr;
-        }
-        return resultArr;
-    }
-
-    public static int[][] findContinuousSequence2(int target) {
         List<int[]> result = new ArrayList<>();
         LinkedList<Integer> queue = new LinkedList<>();
         int start = 1, end = (int) Math.floor(target >> 1) + 1;
@@ -94,9 +68,54 @@ public class leetCode57II {
         return resultArr;
     }
 
+    /**
+     * 暴力求解
+     *
+     * @param target
+     * @return
+     */
+    public static int[][] findContinuousSequence2(int target) {
+        List<List<Integer>> result = new ArrayList<>();
+        for (int i = 1; i < target; i++) {
+            Integer sum = 0;
+            List<Integer> numList = new ArrayList<>();
+            for (int j = i; j < target; j++) {
+                sum = sum + j;
+                if (sum < target) {
+                    numList.add(j);
+                } else if (sum == target) {
+                    numList.add(j);
+                    result.add(numList);
+                    break;
+                } else {
+                    numList = new ArrayList<Integer>();
+                    break;
+                }
+            }
+        }
+        int[][] resultArr = new int[result.size()][];
+        List<Integer> tmpList = null;
+        for (int i = 0; i < result.size(); i++) {
+            tmpList = result.get(i);
+            int[] tmpArr = new int[tmpList.size()];
+            for (int j = 0; j < tmpList.size(); j++) {
+                tmpArr[j] = tmpList.get(j);
+            }
+            resultArr[i] = tmpArr;
+        }
+        return resultArr;
+    }
+
     public static void main(String[] args) {
         int[][] result = findContinuousSequence(15);
         for (int[] tmpArr : result) {
+            for (int tmp : tmpArr) {
+                System.out.print(tmp + ",");
+            }
+            System.out.println(" ");
+        }
+        int[][] result2 = findContinuousSequence2(15);
+        for (int[] tmpArr : result2) {
             for (int tmp : tmpArr) {
                 System.out.print(tmp + ",");
             }
