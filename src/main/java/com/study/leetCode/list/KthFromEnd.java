c33e8235a031a0c656ff168f520851bcb1bc0bb1
wanghongbo <270028806@qq.com>
2020-03-31 20:00:09 +0800

    LeetCode-面试题22-链表中倒数第k个节点

---
+++ src/main/java/com/study/leetCode/list/KthFromEnd.java
@@ -0,0 +1,32 @@
+package main.java.com.study.leetCode.list;
+
+/**
+ * @author: whb
+ * @date: 2020/3/31 19:44
+ * @description: LeetCode-面试题22-链表中倒数第k个节点
+ * 输入一个链表，输出该链表中倒数第k个节点。为了符合大多数人的习惯，本题从1开始计数，即链表的尾节点是倒数第1个节点。例如，一个链表有6个节点，从头节点开始，它们的值依次是1、2、3、4、5、6。这个链表的倒数第3个节点是值为4的节点。
+ * <p>
+ * 示例：
+ * <p>
+ * 给定一个链表: 1->2->3->4->5, 和 k = 2.
+ * <p>
+ * 返回链表 4->5.
+ */
+public class KthFromEnd {
+    public ListNode getKthFromEnd(ListNode head, int k) {
+        ListNode fast = head;
+        while (k-- > 0) {
+            fast = fast.next;
+        }
+        if (fast == null) {
+            return head;
+        }
+        ListNode slow = head;
+        while (fast.next != null) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        slow = slow.next;
+        return slow;
+    }
+}
