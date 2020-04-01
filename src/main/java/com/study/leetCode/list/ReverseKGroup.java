c4511e7010d854628a5f79d5c20fe45b0162104d
wanghongbo <270028806@qq.com>
2020-04-01 17:40:38 +0800

    LeetCode-25-K个一组翻转链表

---
+++ src/main/java/com/study/leetCode/list/ReverseKGroup.java
@@ -0,0 +1,76 @@
+package main.java.com.study.leetCode.list;
+
+/**
+ * @author: whb
+ * @date: 2020/4/1 17:27
+ * @description: LeetCode-25-K个一组翻转链表
+ * 给你一个链表，每 k 个节点一组进行翻转，请你返回翻转后的链表。
+ * <p>
+ * k 是一个正整数，它的值小于或等于链表的长度。
+ * <p>
+ * 如果节点总数不是 k 的整数倍，那么请将最后剩余的节点保持原有顺序。
+ * <p>
+ * 示例：
+ * <p>
+ * 给你这个链表：1->2->3->4->5
+ * <p>
+ * 当 k = 2 时，应当返回: 2->1->4->3->5
+ * <p>
+ * 当 k = 3 时，应当返回: 3->2->1->4->5
+ */
+public class ReverseKGroup {
+    /**
+     * 1、链表分区为已翻转部分+待翻转部分+未翻转部分
+     * 2、每次翻转前，要确定翻转链表的范围，这个必须通过 k 此循环来确定
+     * 3、需记录翻转链表前驱和后继，方便翻转完成后把已翻转部分和未翻转部分连接起来
+     * 4、初始需要两个变量 pre 和 end，pre 代表待翻转链表的前驱，end 代表待翻转链表的末尾
+     * 5、经过k此循环，end 到达末尾，记录待翻转链表的后继 next = end.next
+     * 6、翻转链表，然后将三部分链表连接起来，然后重置 pre 和 end 指针，然后进入下一次循环
+     * 7、特殊情况，当翻转部分长度不足 k 时，在定位 end 完成后，end==null，已经到达末尾，说明题目已完成，直接返回即可
+     *
+     * @param head
+     * @param k
+     * @return
+     */
+    public ListNode reverseKGroup(ListNode head, int k) {
+        //虚拟头结点
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+        //定义前驱结点和结束节点
+        ListNode prev = dummy;
+        ListNode end = dummy;
+        while (end.next != null) {
+            //以k个结点分组
+            for (int i = 0; end != null && i < k; i++) {
+                end = end.next;
+            }
+            //不足k个时不处理
+            if (end == null) {
+                break;
+            }
+            //处理子链表
+            ListNode start = prev.next;
+            ListNode next = end.next;
+            end.next = null;
+            //翻转子链表
+            prev.next = reverse(start);
+            //将子链表前后串起来
+            start.next = next;
+            prev = start;
+            end = prev;
+        }
+        return dummy.next;
+    }
+
+    private ListNode reverse(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+}
