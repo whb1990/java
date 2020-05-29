a5a4e1f40dc364a953c1c6d8335285df11c3ee97
wanghongbo <270028806@qq.com>
2020-05-29 18:57:20 +0800

    LeetCode-94-二叉树的中序遍历（迭代解法）

--- src/main/java/com/study/leetCode/dataStructure/InorderTraversal.java
+++ src/main/java/com/study/leetCode/dataStructure/InorderTraversal.java
@@ -42,6 +42,33 @@ public class InorderTraversal {
         dfs(root.right);
     }
 
+    /**
+     * 迭代解法
+     * 1、中序遍历：左根右。前序遍历：根左右；后序遍历：左右根。
+     * 2、创建一个栈，将根节点和左节点依次入栈
+     * 3、当左节点空时，进行出栈，添加到链表中。此时左节点在前，根节点在后
+     * 4、遍历右节点
+     * 5、重复2、3、4步，直到树为空
+     *
+     * @param root
+     * @return
+     */
+    public List<Integer> inorderTraversal(TreeNode root) {
+        Stack<TreeNode> stack = new Stack<>();
+        List<Integer> list = new ArrayList<>();
+        while (root != null || !stack.isEmpty()) {
+            if (root != null) {
+                stack.push(root);
+                root = root.left;
+            } else {
+                root = stack.pop();
+                list.add(root.val);
+                root = root.right;
+            }
+        }
+        return list;
+    }
+
     class TreeNode {
         int val;
         TreeNode left;
