2129274a139a2bbedcd9b0877758cb62519dd08e
wanghongbo <270028806@qq.com>
2020-05-29 15:35:15 +0800

    LeetCode-494-目标和（动态规划解法）

--- src/main/java/com/study/leetCode/dataStructure/FindTargetSumWays.java
+++ src/main/java/com/study/leetCode/dataStructure/FindTargetSumWays.java
@@ -54,4 +54,36 @@ public class FindTargetSumWays {
         dfs(nums, S - nums[length], length + 1);
         dfs(nums, S + nums[length], length + 1);
     }
+
+    /**
+     * 动态规划解法
+     * sum(P)前面符号为+的集合；sum(N)前面符号为-的集合，所以题目就转化为
+     * sum(P) - sum(N) = target
+     * sum(nums) = sum(P) + sum(N)
+     * ==> 2 * sum(P) = target + sum(nums);
+     * ==> sum(P) = (target + sum(nums)) / 2
+     * 因此题目转化为01背包问题，也就是能组合成容量为sum(P)的方式有多少种
+     *
+     * @param nums
+     * @param S
+     * @return
+     */
+    public int findTargetSumWaysDP(int[] nums, int S) {
+        int sum = 0;
+        for (int num : nums) {
+            sum += num;
+        }
+        if (sum < S || (sum + S) % 2 == 1) {
+            return 0;
+        }
+        int w = (sum + S) / 2;
+        int[] dp = new int[w + 1];
+        dp[0] = 1;
+        for (int num : nums) {
+            for (int j = w; j >= num; j--) {
+                dp[j] += dp[j - num];
+            }
+        }
+        return dp[w];
+    }
 }
