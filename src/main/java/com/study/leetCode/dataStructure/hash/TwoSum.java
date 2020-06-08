42a141cd8ad7a49f30d16d626cdb2a47107ad992
wanghongbo <270028806@qq.com>
2020-06-08 19:07:13 +0800

    LeetCode-1-两数之和

---
+++ src/main/java/com/study/leetCode/dataStructure/hash/TwoSum.java
@@ -0,0 +1,33 @@
+package main.java.com.study.leetCode.dataStructure.hash;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author: whb
+ * @date: 2020/6/8 18:55
+ * @description: LeetCode-1-两数之和
+ * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+ * <p>
+ * 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。
+ */
+public class TwoSum {
+    public static int[] twoSum(int[] nums, int target) {
+        if (nums == null || nums.length < 2) {
+            return new int[]{-1, -1};
+        }
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                return new int[]{map.get(target - nums[i]), i};
+            }
+            map.put(nums[i], i);
+        }
+        return new int[]{-1, -1};
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(twoSum(new int[]{3, 3}, 6)));
+    }
+}
