687d6cbe80dcc1d2e77a91c1e8b30d623a182532
wanghongbo <270028806@qq.com>
2020-06-11 19:44:42 +0800

    LeetCode-94-二叉树的中序遍历（递归/迭代）

--- src/main/java/com/study/leetCode/dataStructure/tree/InorderTraversal.java
+++ src/main/java/com/study/leetCode/dataStructure/tree/InorderTraversal.java
@@ -1,4 +1,4 @@
-package main.java.com.study.leetCode.dataStructure;
+package main.java.com.study.leetCode.dataStructure.tree;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -9,17 +9,18 @@ import java.util.Stack;
  * @date: 2020/5/29 15:17
  * @description: LeetCode-94-二叉树的中序遍历
  * 给定一个二叉树，返回它的中序 遍历。
- * <p>
+ *
  * 示例:
- * <p>
+ *
  * 输入: [1,null,2,3]
- * 1
- * \
- * 2
- * /
- * 3
- * <p>
+ *    1
+ *     \
+ *      2
+ *     /
+ *    3
+ *
  * 输出: [1,3,2]
+ * 进阶: 递归算法很简单，你可以通过迭代算法完成吗？
  */
 public class InorderTraversal {
 
@@ -67,15 +68,5 @@ public class InorderTraversal {
             }
         }
         return list;
-    }
-
-    class TreeNode {
-        int val;
-        TreeNode left;
-        TreeNode right;
-
-        TreeNode(int x) {
-            val = x;
-        }
     }
 }
