cf51abd83b9c6c57e113b55d4329cb1e0691a401
wanghongbo <270028806@qq.com>
2020-06-11 17:08:51 +0800

    LeetCode-3-无重复字符的最长子串（滑动窗口）

---
+++ src/main/java/com/study/leetCode/slidingWindow/LengthOfLongestSubstring.java
@@ -0,0 +1,56 @@
+package main.java.com.study.leetCode.slidingWindow;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author: whb
+ * @date: 2020/6/11 17:02
+ * @description: LeetCode-3-无重复字符的最长子串
+ * 给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。
+ * <p>
+ * 示例 1:
+ * 输入: "abcabcbb"
+ * 输出: 3
+ * 解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
+ * <p>
+ * 示例 2:
+ * 输入: "bbbbb"
+ * 输出: 1
+ * 解释: 因为无重复字符的最长子串是 "b"，所以其长度为 1。
+ * <p>
+ * 示例 3:
+ * 输入: "pwwkew"
+ * 输出: 3
+ * 解释: 因为无重复字符的最长子串是 "wke"，所以其长度为 3。
+ * 请注意，你的答案必须是 子串 的长度，"pwke" 是一个子序列，不是子串。
+ */
+public class LengthOfLongestSubstring {
+    /**
+     * 滑动窗口算法
+     * 定义两个指针，start和end，代表当前窗口的开始和结束位置，使用HashSet存储窗口中的字符。
+     * 当窗口中出现重复的字符时，移除start处的字符，并让start++,没有重复时，添加end处的字符，并让end++,每次更新长度的最大值
+     *
+     * @param s
+     * @return
+     */
+    public static int lengthOfLongestSubstring(String s) {
+        int result = 0, start = 0, end = 0;
+        Set<Character> set = new HashSet<>();
+        while (start < s.length() && end < s.length()) {
+            if (set.contains(s.charAt(end))) {
+                set.remove(s.charAt(start++));
+            } else {
+                set.add(s.charAt(end++));
+                result = Math.max(result, set.size());
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(lengthOfLongestSubstring("abcabcbb"));
+        System.out.println(lengthOfLongestSubstring("bbbbb"));
+        System.out.println(lengthOfLongestSubstring("pwwkew"));
+    }
+}
