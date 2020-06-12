e144d70aed785799413e04335d36baa0ff037bfc
wanghongbo <270028806@qq.com>
2020-06-12 15:00:39 +0800

    LeetCode-101-对称二叉树（迭代）

--- src/main/java/com/study/leetCode/dataStructure/tree/IsSymmetric.java
+++ src/main/java/com/study/leetCode/dataStructure/tree/IsSymmetric.java
@@ -1,29 +1,64 @@
 package main.java.com.study.leetCode.dataStructure.tree;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * @author: whb
  * @date: 2020/6/12 14:45
  * @description: LeetCode-101-对称二叉树
  * 给定一个二叉树，检查它是否是镜像对称的。
- * <p>
+ *
  * 例如，二叉树 [1,2,2,3,4,4,3] 是对称的。
- * <p>
- * 1
- * / \
- * 2   2
- * / \ / \
+ *
+ *     1
+ *    / \
+ *   2   2
+ *  / \ / \
  * 3  4 4  3
- * <p>
- * <p>
+ *
  * 但是下面这个 [1,2,2,null,3,null,3] 则不是镜像对称的:
- * <p>
- * 1
- * / \
- * 2   2
- * \   \
- * 3    3
+ *
+ *     1
+ *    / \
+ *   2   2
+ *    \   \
+ *    3    3
  */
 public class IsSymmetric {
+    /**
+     * 迭代
+     *
+     * @param root
+     * @return
+     */
+    public boolean isSymmetricIteration(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            TreeNode t1 = queue.remove();
+            TreeNode t2 = queue.remove();
+            if (t1 == null && t2 == null) {
+                continue;
+            }
+            if (t1 == null || t2 == null) {
+                return false;
+            }
+            if (t1.val != t2.val) {
+                return false;
+            }
+            queue.add(t1.left);
+            queue.add(t2.right);
+            queue.add(t1.right);
+            queue.add(t2.left);
+        }
+        return true;
+    }
+
     /**
      * 递归调用：n1的左子树等于n2的右子树，n1的右子树等于n2的左子树，一层一层计算
      *
