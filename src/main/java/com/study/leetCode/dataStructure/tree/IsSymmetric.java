c642f418b995cb0e3bb30b1dbfb07bdb8debf1a9
wanghongbo <270028806@qq.com>
2020-06-12 14:52:29 +0800

    LeetCode-101-对称二叉树（递归）

---
+++ src/main/java/com/study/leetCode/dataStructure/tree/IsSymmetric.java
@@ -0,0 +1,49 @@
+package main.java.com.study.leetCode.dataStructure.tree;
+
+/**
+ * @author: whb
+ * @date: 2020/6/12 14:45
+ * @description: LeetCode-101-对称二叉树
+ * 给定一个二叉树，检查它是否是镜像对称的。
+ * <p>
+ * 例如，二叉树 [1,2,2,3,4,4,3] 是对称的。
+ * <p>
+ * 1
+ * / \
+ * 2   2
+ * / \ / \
+ * 3  4 4  3
+ * <p>
+ * <p>
+ * 但是下面这个 [1,2,2,null,3,null,3] 则不是镜像对称的:
+ * <p>
+ * 1
+ * / \
+ * 2   2
+ * \   \
+ * 3    3
+ */
+public class IsSymmetric {
+    /**
+     * 递归调用：n1的左子树等于n2的右子树，n1的右子树等于n2的左子树，一层一层计算
+     *
+     * @param root
+     * @return
+     */
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        return isSymmetric(root.left, root.right);
+    }
+
+    private boolean isSymmetric(TreeNode left, TreeNode right) {
+        if (left == null || right == null) {
+            return left == right;
+        }
+        if (left.val != right.val) {
+            return false;
+        }
+        return isSymmetric(left.left, right.right) && isSymmetric(left.right, right.left);
+    }
+}
