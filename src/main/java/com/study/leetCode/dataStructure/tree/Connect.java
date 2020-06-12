ce2e21eab528c6f26cdb84c3c0b171b9013f2df9
wanghongbo <270028806@qq.com>
2020-06-12 18:29:44 +0800

    LeetCode-116-填充每个节点的下一个右侧节点指针（层序遍历）

---
+++ src/main/java/com/study/leetCode/dataStructure/tree/Connect.java
@@ -0,0 +1,79 @@
+package main.java.com.study.leetCode.dataStructure.tree;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * @author: whb
+ * @date: 2020/6/12 18:21
+ * @description: LeetCode-116-填充每个节点的下一个右侧节点指针
+ * 给定一个完美二叉树，其所有叶子节点都在同一层，每个父节点都有两个子节点。二叉树定义如下：
+ *
+ * struct Node {
+ *   int val;
+ *   Node *left;
+ *   Node *right;
+ *   Node *next;
+ * }
+ * 填充它的每个 next 指针，让这个指针指向其下一个右侧节点。如果找不到下一个右侧节点，则将 next 指针设置为 NULL。
+ *
+ * 初始状态下，所有 next 指针都被设置为 NULL。
+ * 输入：{"$id":"1","left":{"$id":"2","left":{"$id":"3","left":null,"next":null,"right":null,"val":4},"next":null,"right":{"$id":"4","left":null,"next":null,"right":null,"val":5},"val":2},"next":null,"right":{"$id":"5","left":{"$id":"6","left":null,"next":null,"right":null,"val":6},"next":null,"right":{"$id":"7","left":null,"next":null,"right":null,"val":7},"val":3},"val":1}
+ *
+ * 输出：{"$id":"1","left":{"$id":"2","left":{"$id":"3","left":null,"next":{"$id":"4","left":null,"next":{"$id":"5","left":null,"next":{"$id":"6","left":null,"next":null,"right":null,"val":7},"right":null,"val":6},"right":null,"val":5},"right":null,"val":4},"next":{"$id":"7","left":{"$ref":"5"},"next":null,"right":{"$ref":"6"},"val":3},"right":{"$ref":"4"},"val":2},"next":null,"right":{"$ref":"7"},"val":1}
+ *
+ * 解释：给定二叉树如图 A 所示，你的函数应该填充它的每个 next 指针，以指向其下一个右侧节点，如图 B 所示。
+ */
+public class Connect {
+    /**
+     * 层序遍历，将每层的最右边的下一个节点赋值为null；
+     * 将上一个节点指向下一个节点
+     *
+     * @param root
+     * @return
+     */
+    public Node connect(Node root) {
+        if (root == null) {
+            return root;
+        }
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            Node next = null;
+            while (size-- > 0) {
+                Node curr = queue.remove();
+                if (curr.right != null) {
+                    queue.add(curr.right);
+                }
+                if (curr.left != null) {
+                    queue.add(curr.left);
+                }
+                curr.next = next;
+                next = curr;
+            }
+        }
+        return root;
+    }
+
+    class Node {
+        public int val;
+        public Node left;
+        public Node right;
+        public Node next;
+
+        public Node() {
+        }
+
+        public Node(int _val) {
+            this.val = _val;
+        }
+
+        public Node(int _val, Node _left, Node _right, Node _next) {
+            this.val = _val;
+            this.left = _left;
+            this.right = _right;
+            this.next = _next;
+        }
+    }
+}
