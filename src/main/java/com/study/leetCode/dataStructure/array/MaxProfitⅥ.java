5dc928904d2f8f2d0a1000df22c949d3ffdc3a45
wanghongbo <270028806@qq.com>
2020-07-03 19:19:55 +0800

    LeetCode-714-买卖股票的最佳时机含手续费（动态规划）

--- src/main/java/com/study/leetCode/dataStructure/array/MaxProfitⅥ.java
+++ src/main/java/com/study/leetCode/dataStructure/array/MaxProfitⅥ.java
@@ -46,7 +46,30 @@ public class MaxProfitⅥ {
         return cash;
     }
 
+    /**
+     * 动态规划
+     *
+     * @param prices
+     * @param fee
+     * @return
+     */
+    public static int maxProfitDP(int[] prices, int fee) {
+        if (prices.length <= 1) {
+            return 0;
+        }
+        //定义二维数组，dp[i][0]表示不持有股票时的收益，dp[i][1]代表持有股票时的收益
+        int[][] dp = new int[prices.length][2];
+        dp[0][0] = 0;
+        dp[0][1] = -prices[0] - fee;
+        for (int i = 1; i < prices.length; i++) {
+            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
+            dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] - prices[i] - fee);
+        }
+        return dp[prices.length - 1][0];
+    }
+
     public static void main(String[] args) {
         System.out.println(maxProfit(new int[]{1, 3, 2, 8, 4, 9}, 2));
+        System.out.println(maxProfitDP(new int[]{1, 3, 2, 8, 4, 9}, 2));
     }
 }
