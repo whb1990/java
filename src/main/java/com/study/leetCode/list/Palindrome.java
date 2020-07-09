dbc888745d4d5593114f5694f902f3c1de541595
wanghongbo <270028806@qq.com>
2020-07-09 18:12:51 +0800

    LeetCode-234-回文链表（利用栈解决）

--- src/main/java/com/study/leetCode/list/Palindrome.java
+++ src/main/java/com/study/leetCode/list/Palindrome.java
@@ -1,9 +1,12 @@
 package main.java.com.study.leetCode.list;
 
+import java.util.Stack;
+
 /**
  * @author: whb
  * @date: 2020/3/31 17:11
  * @description: LeetCode-234-回文链表
+ * 难度：简单
  * 请判断一个链表是否为回文链表。
  * <p>
  * 示例 1:
@@ -68,6 +71,66 @@ public class Palindrome {
             }
             l1 = l1.next;
             l2 = l2.next;
+        }
+        return true;
+    }
+
+    /**
+     * 解法二
+     * 利用栈来做辅助，把链表的节点全部入栈，再一个个出栈与链表进行对比
+     *
+     * @param head
+     * @return
+     */
+    public boolean isPalindrome2(ListNode head) {
+        if (head == null || head.next == null) {
+            return true;
+        }
+        Stack<ListNode> stack = new Stack<>();
+        ListNode curr = head;
+        while (curr != null) {
+            stack.push(curr);
+            curr = curr.next;
+        }
+        while (!stack.isEmpty()) {
+            ListNode tmp = stack.pop();
+            if (tmp.val != head.val) {
+                return false;
+            }
+            head = head.next;
+        }
+        return true;
+    }
+
+    /**
+     * 解法三
+     * 让链表的后半部分入栈就可以了，然后把栈中的元素与链表的前半部分对比
+     *
+     * @param head
+     * @return
+     */
+    public boolean isPalindrome3(ListNode head) {
+        if (head == null || head.next == null) {
+            return true;
+        }
+        //快慢指针
+        ListNode slow = head, fast = head;
+        Stack<ListNode> stack = new Stack<>();
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        slow = slow.next;
+        while (slow != null) {
+            stack.push(slow);
+            slow = slow.next;
+        }
+        while (!stack.isEmpty()) {
+            ListNode tmp = stack.pop();
+            if (tmp.val != head.val) {
+                return false;
+            }
+            head = head.next;
         }
         return true;
     }
