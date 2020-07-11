e47bd0cebc7049725ddd56e09f7f677ee1a46c62
wanghongbo <270028806@qq.com>
2020-07-11 10:50:59 +0800

    LeetCode-114-二叉树展开为链表

---
+++ src/main/java/com/study/leetCode/list/TreeToList.java
@@ -0,0 +1,56 @@
+package main.java.com.study.leetCode.list;
+
+import main.java.com.study.leetCode.dataStructure.tree.TreeNode;
+
+/**
+ * @author: whb
+ * @date: 2020/7/11 10:46
+ * @description: LeetCode-114-二叉树展开为链表
+ * 难度：中等
+ * 给定一个二叉树，原地将它展开为一个单链表。
+ *
+ * 例如，给定二叉树
+ *
+ *     1
+ *    / \
+ *   2   5
+ *  / \   \
+ * 3   4   6
+ * 将其展开为：
+ *
+ * 1
+ *  \
+ *   2
+ *    \
+ *     3
+ *      \
+ *       4
+ *        \
+ *         5
+ *          \
+ *           6
+ */
+public class TreeToList {
+    /**
+     * 展开的顺序其实就是二叉树的先序遍历。
+     * 1、将左子树插入到右子树的地方；
+     * 2、将原来的右子树插入到左子树的左右边节点；
+     * 3、考虑新的右子树的根节点，一直重复上述过程，直到新的右子树为null。
+     *
+     * @param root
+     */
+    public void flattern(TreeNode root) {
+        while (root != null) {
+            if (root.left != null) {
+                TreeNode pre = root.left;
+                while (pre.right != null) {
+                    pre = pre.right;
+                }
+                pre.right = root.right;
+                root.right = root.left;
+                root.left = null;
+            }
+            root = root.right;
+        }
+    }
+}
