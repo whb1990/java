a0276bf51f711ff1e69e181545964188e8c1922b
wanghongbo <270028806@qq.com>
2020-07-23 15:35:58 +0800

    LeetCode-410-分割数组的最大值（二分查找）

--- src/main/java/com/study/leetCode/binarySearch/SplitArray.java
+++ src/main/java/com/study/leetCode/binarySearch/SplitArray.java
@@ -4,6 +4,7 @@ package main.java.com.study.leetCode.binarySearch;
  * @author: whb
  * @date: 2020/6/10 18:02
  * @description: LeetCode-410-分割数组的最大值
+ * 难度：困难
  * 给定一个非负整数数组和一个整数 m，你需要将这个数组分成 m 个非空的连续子数组。设计一个算法使得这 m 个子数组各自和的最大值最小。
  * <p>
  * 注意:
@@ -11,8 +12,8 @@ package main.java.com.study.leetCode.binarySearch;
  * <p>
  * 1 ≤ n ≤ 1000
  * 1 ≤ m ≤ min(50, n)
- * 示例:
  * <p>
+ * 示例:
  * 输入:
  * nums = [7,2,5,10,8]
  * m = 2
