0415ca574dd61c503b51fdfe3666572b2854881d
wanghongbo <270028806@qq.com>
2020-07-30 17:54:39 +0800

    LeetCode-1-两数之和（增加测试用例）

--- src/main/java/com/study/leetCode/dataStructure/hash/TwoSum.java
+++ src/main/java/com/study/leetCode/dataStructure/hash/TwoSum.java
@@ -29,5 +29,6 @@ public class TwoSum {
 
     public static void main(String[] args) {
         System.out.println(Arrays.toString(twoSum(new int[]{3, 3}, 6)));
+        System.out.println(Arrays.toString(twoSum(new int[]{2, 7, 11, 15}, 9)));
     }
 }
