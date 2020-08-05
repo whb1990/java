7b6bfa9aed25be718a45d78080de894b11c91980
wanghongbo <270028806@qq.com>
2020-08-05 14:18:12 +0800

    LeetCode-198-打家劫舍（动态规划）

---
+++ src/main/java/com/study/leetCode/dp/Rob.java
@@ -0,0 +1,63 @@
+package main.java.com.study.leetCode.dp;
+
+/**
+ * @author: whb
+ * @date: 2020/8/5 12:04
+ * @description: LeetCode-198-打家劫舍
+ * 难度：简单
+ * 你是一个专业的小偷，计划偷窃沿街的房屋。每间房内都藏有一定的现金，影响你偷窃的唯一制约因素就是相邻的房屋装有相互连通的防盗系统，如果两间相邻的房屋在同一晚上被小偷闯入，系统会自动报警。
+ * <p>
+ * 给定一个代表每个房屋存放金额的非负整数数组，计算你 不触动警报装置的情况下 ，一夜之内能够偷窃到的最高金额。
+ * <p>
+ * 示例 1：
+ * 输入：[1,2,3,1]
+ * 输出：4
+ * 解释：偷窃 1 号房屋 (金额 = 1) ，然后偷窃 3 号房屋 (金额 = 3)。
+ *      偷窃到的最高金额 = 1 + 3 = 4 。
+ * <p>
+ * 示例 2：
+ * 输入：[2,7,9,3,1]
+ * 输出：12
+ * 解释：偷窃 1 号房屋 (金额 = 2), 偷窃 3 号房屋 (金额 = 9)，接着偷窃 5 号房屋 (金额 = 1)。
+ *      偷窃到的最高金额 = 2 + 9 + 1 = 12 。
+ * <p>
+ * 提示：
+ * 0 <= nums.length <= 100
+ * 0 <= nums[i] <= 400
+ */
+public class Rob {
+    /**
+     * 动态规划
+     *
+     * @param nums
+     * @return
+     */
+    public static int rob(int[] nums) {
+        //如果数组为0
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        //如果只有这一家 那么偷他
+        if (nums.length == 1) {
+            return nums[0];
+        }
+        //如果有两家 偷富的一家
+        if (nums.length == 2) {
+            return Math.max(nums[0], nums[1]);
+        }
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+        dp[1] = Math.max(dp[0], nums[1]);
+        //遍历进行动态规划
+        for (int i = 2; i < nums.length; i++) {
+            dp[i] = Math.max(dp[i - 2] + nums[i], dp[i - 1]);
+        }
+        return dp[nums.length - 1];
+    }
+
+    public static void main(String[] args) {
+        System.out.println(rob(new int[]{1, 2, 3, 1}));
+        System.out.println(rob(new int[]{2, 7, 9, 3, 1}));
+        System.out.println(rob(new int[]{2, 1, 1, 2}));
+    }
+}
