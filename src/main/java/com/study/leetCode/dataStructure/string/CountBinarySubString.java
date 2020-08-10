63bb92a10c3529306fd9437b557c5fa182656928
wanghongbo <270028806@qq.com>
2020-08-10 10:12:26 +0800

    LeetCode-696-计数二进制子串（按字符分组）

--- src/main/java/com/study/leetCode/dataStructure/string/CountBinarySubString.java
+++ src/main/java/com/study/leetCode/dataStructure/string/CountBinarySubString.java
@@ -1,5 +1,8 @@
 package main.java.com.study.leetCode.dataStructure.string;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author: whb
  * @date: 2020/8/10 9:58
@@ -47,9 +50,42 @@ public class CountBinarySubString {
         return result;
     }
 
+    /**
+     * 按字符分组
+     * 将字符串 s 按照 0 和 1 的连续段分组，存在 counts数组中，例如 s = 00111011，可以得到这样的 counts数组：counts={2,3,1,2}。
+     * 这里counts 数组中两个相邻的数一定代表的是两种不同的字符。假设 counts 数组中两个相邻的数字为 u 或者 v，它们对应着 u 个 0 和 v 个 1，或者 u 个 1 和 v 个 0。
+     * 它们能组成的满足条件的子串数目为 min{u,v}，即一对相邻的数字对答案的贡献。
+     * <p>
+     * 只要遍历所有相邻的数对，求它们的贡献总和，即可得到答案。
+     *
+     * @param s
+     * @return
+     */
+    public static int countBinarySubString2(String s) {
+        int ptr = 0, len = s.length();
+        List<Integer> counts = new ArrayList<>();
+        while (ptr < len) {
+            int count = 0;
+            char c = s.charAt(ptr);
+            while (ptr < len && s.charAt(ptr) == c) {
+                ++ptr;
+                ++count;
+            }
+            counts.add(count);
+        }
+        int result = 0;
+        for (int i = 1; i < counts.size(); i++) {
+            result += Math.min(counts.get(i), counts.get(i - 1));
+        }
+        return result;
+    }
+
     public static void main(String[] args) {
         System.out.println(countBinarySubString("00110011"));
         System.out.println(countBinarySubString("10101"));
         System.out.println(countBinarySubString("00111011"));
+        System.out.println(countBinarySubString2("00110011"));
+        System.out.println(countBinarySubString2("10101"));
+        System.out.println(countBinarySubString2("00111011"));
     }
 }
