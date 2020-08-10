2644e74ae1151722c79dfbfdc0a67472650d9f3d
wanghongbo <270028806@qq.com>
2020-08-10 10:06:12 +0800

    LeetCode-696-计数二进制子串（双指针法）

---
+++ src/main/java/com/study/leetCode/dataStructure/string/CountBinarySubString.java
@@ -0,0 +1,55 @@
+package main.java.com.study.leetCode.dataStructure.string;
+
+/**
+ * @author: whb
+ * @date: 2020/8/10 9:58
+ * @description: LeetCode-696-计数二进制子串
+ * 难度：简单
+ * 给定一个字符串 s，计算具有相同数量0和1的非空(连续)子字符串的数量，并且这些子字符串中的所有0和所有1都是组合在一起的。
+ * <p>
+ * 重复出现的子串要计算它们出现的次数。
+ * <p>
+ * 示例 1 :
+ * 输入: "00110011"
+ * 输出: 6
+ * 解释: 有6个子串具有相同数量的连续1和0：“0011”，“01”，“1100”，“10”，“0011” 和 “01”。
+ * <p>
+ * 请注意，一些重复出现的子串要计算它们出现的次数。
+ * <p>
+ * 另外，“00110011”不是有效的子串，因为所有的0（和1）没有组合在一起。
+ * <p>
+ * 示例 2 :
+ * 输入: "10101"
+ * 输出: 4
+ * 解释: 有4个子串：“10”，“01”，“10”，“01”，它们具有相同数量的连续1和0。
+ */
+public class CountBinarySubString {
+    /**
+     * 双指针法
+     * pre用于统计前面出现的最大次数，cur用来统计当前出现的次数，若cur<=pre则表明可以使得res++
+     *
+     * @param s
+     * @return
+     */
+    public static int countBinarySubString(String s) {
+        int pre = 0, cur = 1, result = 0, len = s.length();
+        for (int i = 1; i < len; i++) {
+            if (s.charAt(i) == s.charAt(i - 1)) {
+                cur++;
+            } else {
+                pre = cur;
+                cur = 1;
+            }
+            if (pre >= cur) {
+                result++;
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(countBinarySubString("00110011"));
+        System.out.println(countBinarySubString("10101"));
+        System.out.println(countBinarySubString("00111011"));
+    }
+}
