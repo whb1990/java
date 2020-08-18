d23deae6e003510da975465174bde919cb34a906
wanghongbo <270028806@qq.com>
2020-08-18 10:56:03 +0800

    LeetCode-18-四数之和（增加测试用例）

--- src/main/java/com/study/leetCode/doublePointer/FourSum.java
+++ src/main/java/com/study/leetCode/doublePointer/FourSum.java
@@ -11,19 +11,19 @@ import java.util.List;
  * 难度：中等
  * 标签：数组、哈希表、双指针
  * 给定一个包含 n 个整数的数组 nums 和一个目标值 target，判断 nums 中是否存在四个元素 a，b，c 和 d ，使得 a + b + c + d 的值与 target 相等？找出所有满足条件且不重复的四元组。
- *
+ * <p>
  * 注意：
- *
+ * <p>
  * 答案中不可以包含重复的四元组。
- *
+ * <p>
  * 示例：
  * 给定数组 nums = [1, 0, -1, 0, -2, 2]，和 target = 0。
- *
+ * <p>
  * 满足要求的四元组集合为：
  * [
- *   [-1,  0, 0, 1],
- *   [-2, -1, 1, 2],
- *   [-2,  0, 0, 2]
+ * [-1,  0, 0, 1],
+ * [-2, -1, 1, 2],
+ * [-2,  0, 0, 2]
  * ]
  */
 public class FourSum {
@@ -57,7 +57,7 @@ public class FourSum {
             //第二层循环j，初始值指向i+1
             for (int j = i + 1; j < len - 2; j++) {
                 //当j的值与前面的值相等时忽略
-                if (j > i - 1 && nums[j] == nums[j - 1]) {
+                if (j > i + 1 && nums[j] == nums[j - 1]) {
                     continue;
                 }
                 //定义指针m指向j+1，指针n指向数组末尾
@@ -101,5 +101,7 @@ public class FourSum {
 
     public static void main(String[] args) {
         System.out.println(fourSum(new int[]{1, 0, -1, 0, -2, 2}, 0));
+        System.out.println(fourSum(new int[]{1, -2, -5, -4, -3, 3, 3, 5}, -11));
+        System.out.println(fourSum(new int[]{0, 0, 0, 0}, 0));
     }
 }
