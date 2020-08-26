fd7604752c889b14b1933fb44b5b1809cabacafd
wanghongbo <270028806@qq.com>
2020-08-26 17:07:33 +0800

    LeetCode-64-最小路径和（动态规划-二维数组）

--- src/main/java/com/study/leetCode/dp/MinPathSum.java
+++ src/main/java/com/study/leetCode/dp/MinPathSum.java
@@ -5,6 +5,7 @@ package main.java.com.study.leetCode.dp;
  * @date: 2020/7/6 17:53
  * @description: LeetCode-64-最小路径和
  * 难度：中等
+ * 标签：数组、动态规划
  * 给定一个包含非负整数的 m x n 网格，请找出一条从左上角到右下角的路径，使得路径上的数字总和为最小。
  *
  * 说明：每次只能向下或者向右移动一步。
@@ -23,9 +24,9 @@ public class MinPathSum {
     /**
      * 步骤一、定义数组元素的含义
      *
-     * 由于我们的目的是从左上角到右下角，最小路径和是多少，那我们就定义 dp[i] [j]的含义为：当机器人从左上角走到(i, j) 这个位置时，最下的路径和是 dp[i] [j]。那么，dp[m-1] [n-1] 就是我们要的答案了。
+     * 由于我们的目的是从左上角到右下角，最小路径和是多少，那我们就定义 dp[i] [j]的含义为：当机器人从左上角走到(i, j) 这个位置时，最小的路径和是 dp[i] [j]。那么，dp[m-1] [n-1] 就是我们要的答案了。
      *
-     * 注意，这个网格相当于一个二维数组，数组是从下标为 0 开始算起的，所以 由下角的位置是 (m-1, n - 1)，所以 dp[m-1] [n-1] 就是我们要走的答案。
+     * 注意，这个网格相当于一个二维数组，数组是从下标为 0 开始算起的，所以 右下角的位置是 (m-1, n - 1)，所以 dp[m-1] [n-1] 就是我们要走的答案。
      *
      * 步骤二：找出关系数组元素间的关系式
      *
@@ -49,27 +50,28 @@ public class MinPathSum {
      * @return
      */
     public static int minPathSum(int[][] grid) {
-        if (grid == null || grid.length == 0 || grid[0] == null || grid[0].length == 0) {
+        if (grid == null || grid.length == 0) {
             return 0;
         }
         //计算行数、列数
         int row = grid.length, col = grid[0].length;
         //定义二维数组
         int[][] dp = new int[row][col];
-        //第一个位置的值 = 网格起点的值
-        dp[0][0] = grid[0][0];
-        //计算第一列的dp，只能向下走
-        for (int i = 1; i < row; i++) {
-            dp[i][0] = dp[i - 1][0] + grid[i][0];
-        }
-        //计算第一行的dp，只能向右走
-        for (int j = 1; j < col; j++) {
-            dp[0][j] = dp[0][j - 1] + grid[0][j];
-        }
-        //计算其他位置的dp
-        for (int i = 1; i < row; i++) {
-            for (int j = 1; j < col; j++) {
-                dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j];
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                //第一个位置的值 = 网格起点的值
+                if (i == 0 && j == 0) {
+                    dp[i][j] = grid[i][j];
+                } else if (i == 0) {
+                    //计算第一行的dp，只能向右走
+                    dp[i][j] = dp[i][j - 1] + grid[i][j];
+                } else if (j == 0) {
+                    //计算第一列的dp，只能向下走
+                    dp[i][j] = dp[i - 1][j] + grid[i][j];
+                } else {
+                    //计算其他位置的dp
+                    dp[i][j] = Math.min(dp[i][j - 1], dp[i - 1][j]) + grid[i][j];
+                }
             }
         }
         //返回最后结果
