e4350fa48f7e3df98b7116f659c159e03461abd8
wanghongbo <270028806@qq.com>
2020-10-27 17:57:06 +0800

    LeetCode-208-实现Trie（前缀树）

---
+++ src/main/java/com/study/leetCode/dataStructure/trie/Trie.java
@@ -0,0 +1,86 @@
+package main.java.com.study.leetCode.dataStructure.trie;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author： whb
+ * @description： LeetCode-208-实现Trie（前缀树）
+ * @date： 2020-10-27 17:41
+ * 难度：中等
+ * 标签：设计、字典树
+ * 实现一个 Trie (前缀树)，包含 insert, search, 和 startsWith 这三个操作。
+ * <p>
+ * 示例:
+ * <p>
+ * Trie trie = new Trie();
+ * <p>
+ * trie.insert("apple");
+ * trie.search("apple");   // 返回 true
+ * trie.search("app");     // 返回 false
+ * trie.startsWith("app"); // 返回 true
+ * trie.insert("app");
+ * trie.search("app");     // 返回 true
+ * 说明:
+ * <p>
+ * 你可以假设所有的输入都是由小写字母 a-z 构成的。
+ * 保证所有输入均为非空字符串。
+ */
+public class Trie {
+    private TrieNode root;
+
+    public Trie() {
+        root = new TrieNode();
+    }
+
+    public void insert(String word) {
+        TrieNode cur = root;
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            if (cur.childrenMap.get(c) == null) {
+                cur.childrenMap.put(c, new TrieNode());
+            }
+            cur = cur.childrenMap.get(c);
+        }
+        cur.isWord = true;
+    }
+
+    public boolean search(String word) {
+        TrieNode cur = root;
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            if (cur.childrenMap.get(c) == null) {
+                return false;
+            }
+            cur = cur.childrenMap.get(c);
+        }
+        return cur.isWord;
+    }
+
+    public boolean startsWith(String prefix) {
+        TrieNode cur = root;
+        for (int i = 0; i < prefix.length(); i++) {
+            char c = prefix.charAt(i);
+            if (cur.childrenMap.get(c) == null) {
+                return false;
+            }
+            cur = cur.childrenMap.get(c);
+        }
+        return true;
+    }
+
+    class TrieNode {
+        public boolean isWord;
+        public Map<Character, TrieNode> childrenMap = new HashMap<>();
+    }
+
+    public static void main(String[] args) {
+        Trie trie = new Trie();
+        trie.insert("apple");
+        System.out.println(trie.search("apple"));
+        System.out.println(trie.search("app"));
+        System.out.println(trie.startsWith("app"));
+        trie.insert("app");
+        System.out.println(trie.startsWith("app"));
+    }
+}
