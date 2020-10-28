9ca3acf0fdc459b8f15599e46976c9e89df3d28f
wanghongbo <270028806@qq.com>
2020-10-28 17:50:14 +0800

    LeetCode-42-接雨水（单调栈）

--- src/main/java/com/study/leetCode/dp/Trap.java
+++ src/main/java/com/study/leetCode/dp/Trap.java
@@ -1,5 +1,7 @@
 package main.java.com.study.leetCode.dp;
 
+import java.util.Stack;
+
 /**
  * @author： whb
  * @description： LeetCode-42-接雨水
@@ -27,8 +29,9 @@ public class Trap {
      * 2、分别从两头遍历height数组，为 dp[i][0]和 dp[i][1] 赋值。
      * <p>
      * 3、遍历每个柱子，累加每个柱子可以储水的高度。
-     *
+     * <p>
      * 时间O(N) 空间O(N)
+     *
      * @param height
      * @return
      */
@@ -60,21 +63,22 @@ public class Trap {
 
     /**
      * 双指针
-     *在上述的动态规划方法中，我们用二维数组来存储每个柱子左右两侧的最大高度，但递推累加每个柱子的储水高度时其实只用到了 dp[i][0]和 dp[i][1] 两个值，因此我们递推的时候只需要用 int leftMax 和 int rightMax 两个变量就行了。
-     *
+     * 在上述的动态规划方法中，我们用二维数组来存储每个柱子左右两侧的最大高度，但递推累加每个柱子的储水高度时其实只用到了 dp[i][0]和 dp[i][1] 两个值，因此我们递推的时候只需要用 int leftMax 和 int rightMax 两个变量就行了。
+     * <p>
      * 即将上述代码中的递推公式：
-     *
+     * <p>
      * res += Math.min(dp[i][0], dp[i][1]) - height[i];
-     *
+     * <p>
      * 优化成：
-     *
+     * <p>
      * res += Math.min(leftMax, rightMax) - height[i];
-     *
+     * <p>
      * 注意这里的 leftMax 是从左端开始递推得到的，而 rightMax 是从右端开始递推得到的。
-     *
+     * <p>
      * 因此遍历每个柱子，累加每个柱子的储水高度时，也需要用 left 和 right 两个指针从两端开始遍历。
-     *
+     * <p>
      * 时间O(N) 空间O(1)
+     *
      * @param height
      * @return
      */
@@ -92,6 +96,53 @@ public class Trap {
         return res;
     }
 
+    /**
+     * 单调递减栈
+     * 用栈来跟踪可能储水的最长的条形块。使用栈就可以在一次遍历内完成计算。
+     * <p>
+     * 在遍历数组时维护一个栈。如果当前的条形块小于或等于栈顶的条形块，将条形块的索引入栈，意思是当前的条形块被栈中的前一个条形块界定。
+     * 如果发现一个条形块长于栈顶，可以确定栈顶的条形块被当前条形块和栈的前一个条形块界定，因此可以弹出栈顶元素并且累加答案到 res 。
+     * <p>
+     * 算法
+     * <p>
+     * 使用栈来存储条形块的索引下标。
+     * 遍历数组：
+     * 当栈非空且 height[current]>height[st.top()]
+     * 意味着栈中元素可以被弹出。弹出栈顶元素 top。
+     * 计算当前元素和栈顶元素的距离，准备进行填充操作
+     * distance=current−st.top()−1
+     * 找出界定高度
+     * bounded_height=min(height[current],height[st.top()])−height[top]
+     * 往答案中累加积水量 res+=distance×bounded_height
+     * 将当前索引下标入栈
+     * 将 current 移动到下个位置
+     *
+     * @param height
+     * @return
+     */
+    public static int trapStack(int[] height) {
+        Stack<Integer> stack = new Stack<>();
+        int res = 0;
+        // 遍历每个柱体
+        for (int i = 0; i < height.length; i++) {
+            while (!stack.isEmpty() && height[i] > height[stack.peek()]) {
+                int bottomIdx = stack.peek();
+                // 如果栈顶元素一直相等，那么全都pop出去，只留第一个。
+                while (!stack.isEmpty() && height[stack.peek()] == height[bottomIdx]) {
+                    stack.pop();
+                }
+                if (!stack.isEmpty()) {
+                    // stack.peek()是此次接住的雨水的左边界的位置，右边界是当前的柱体，即i。
+                    // Math.min(height[stack.peek()], height[i]) 是左右柱子高度的min，减去height[bottomIdx]就是雨水的高度。
+                    // i - stack.peek() - 1 是雨水的宽度。
+                    res += (Math.min(height[stack.peek()], height[i]) - height[bottomIdx]) * (i - stack.peek() - 1);
+                }
+            }
+            stack.push(i);
+        }
+        return res;
+    }
+
     public static void main(String[] args) {
         System.out.println("===============动态规划===================");
         System.out.println(trapDp(new int[]{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1}));
@@ -99,5 +150,8 @@ public class Trap {
         System.out.println("===============双指针===================");
         System.out.println(trapDoublePointer(new int[]{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1}));
         System.out.println(trapDoublePointer(new int[]{4, 2, 0, 3, 2, 5}));
+        System.out.println("===============单调栈===================");
+        System.out.println(trapStack(new int[]{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1}));
+        System.out.println(trapStack(new int[]{4, 2, 0, 3, 2, 5}));
     }
 }
