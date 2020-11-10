3e617e37e7176624e8fcf01938a731bc7d9ad5a2
wanghongbo <270028806@qq.com>
2020-11-10 11:31:04 +0800

    LeetCode-114-二叉树展开为链表（递归）

--- src/main/java/com/study/leetCode/list/TreeToList.java
+++ src/main/java/com/study/leetCode/list/TreeToList.java
@@ -7,6 +7,7 @@ import main.java.com.study.leetCode.dataStructure.tree.TreeNode;
  * @date: 2020/7/11 10:46
  * @description: LeetCode-114-二叉树展开为链表
  * 难度：中等
+ * 标签：树、深度优先搜索
  * 给定一个二叉树，原地将它展开为一个单链表。
  *
  * 例如，给定二叉树
@@ -32,6 +33,7 @@ import main.java.com.study.leetCode.dataStructure.tree.TreeNode;
  */
 public class TreeToList {
     /**
+     * 迭代
      * 展开的顺序其实就是二叉树的先序遍历。
      * 1、将左子树插入到右子树的地方；
      * 2、将原来的右子树插入到左子树的左右边节点；
@@ -53,4 +55,25 @@ public class TreeToList {
             root = root.right;
         }
     }
+
+    /**
+     * 递归
+     */
+    public void flattern2(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        flattern2(root.left);
+        flattern2(root.right);
+        TreeNode left = root.left;
+        TreeNode right = root.right;
+        root.left = null;
+        root.right = left;
+        TreeNode p = root;
+        while (p.right != null) {
+            p = p.right;
+        }
+        p.right = right;
+    }
+
 }
