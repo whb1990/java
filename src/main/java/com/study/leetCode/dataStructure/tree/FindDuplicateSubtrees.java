688390eeb438d3c2cf2fef6fad6538d5fcdad5fe
wanghongbo <270028806@qq.com>
2020-11-10 17:10:10 +0800

    LeetCode-652-寻找重复子树

--- src/main/java/com/study/leetCode/dataStructure/tree/FindDuplicateSubtrees.java
+++ src/main/java/com/study/leetCode/dataStructure/tree/FindDuplicateSubtrees.java
@@ -1,4 +1,4 @@
-package main.java.com.study.leetCode.dataStructure.hash;
+package main.java.com.study.leetCode.dataStructure.tree;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -9,6 +9,8 @@ import java.util.Map;
  * @author: whb
  * @date: 2020/6/11 14:37
  * @description: LeetCode-652-寻找重复子树
+ * 难度：中等
+ * 标签：树
  * 给定一棵二叉树，返回所有重复的子树。对于同一类的重复子树，你只需要返回其中任意一棵的根结点即可。
  *
  * 两棵树重复是指它们具有相同的结构以及相同的结点值。
@@ -73,24 +75,5 @@ public class FindDuplicateSubtrees {
             result.add(root);
         }
         return serial;
-    }
-
-    class TreeNode {
-        int val;
-        TreeNode left;
-        TreeNode right;
-
-        TreeNode() {
-        }
-
-        TreeNode(int val) {
-            this.val = val;
-        }
-
-        TreeNode(int val, TreeNode left, TreeNode right) {
-            this.val = val;
-            this.left = left;
-            this.right = right;
-        }
     }
 }
