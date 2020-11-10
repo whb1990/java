a5d07ee3b8e150c5ba8d09dd2d349e1cd4a75c52
wanghongbo <270028806@qq.com>
2020-11-10 11:20:56 +0800

    LeetCode-116-填充每个节点的下一个右侧节点指针（递归）

--- src/main/java/com/study/leetCode/dataStructure/tree/Connect.java
+++ src/main/java/com/study/leetCode/dataStructure/tree/Connect.java
@@ -7,25 +7,28 @@ import java.util.Queue;
  * @author: whb
  * @date: 2020/6/12 18:21
  * @description: LeetCode-116-填充每个节点的下一个右侧节点指针
+ * 难度：中等
+ * 标签：树、深度优先搜索、广度优先搜索
  * 给定一个完美二叉树，其所有叶子节点都在同一层，每个父节点都有两个子节点。二叉树定义如下：
- *
+ * <p>
  * struct Node {
- *   int val;
- *   Node *left;
- *   Node *right;
- *   Node *next;
+ * int val;
+ * Node *left;
+ * Node *right;
+ * Node *next;
  * }
  * 填充它的每个 next 指针，让这个指针指向其下一个右侧节点。如果找不到下一个右侧节点，则将 next 指针设置为 NULL。
- *
+ * <p>
  * 初始状态下，所有 next 指针都被设置为 NULL。
  * 输入：{"$id":"1","left":{"$id":"2","left":{"$id":"3","left":null,"next":null,"right":null,"val":4},"next":null,"right":{"$id":"4","left":null,"next":null,"right":null,"val":5},"val":2},"next":null,"right":{"$id":"5","left":{"$id":"6","left":null,"next":null,"right":null,"val":6},"next":null,"right":{"$id":"7","left":null,"next":null,"right":null,"val":7},"val":3},"val":1}
- *
+ * <p>
  * 输出：{"$id":"1","left":{"$id":"2","left":{"$id":"3","left":null,"next":{"$id":"4","left":null,"next":{"$id":"5","left":null,"next":{"$id":"6","left":null,"next":null,"right":null,"val":7},"right":null,"val":6},"right":null,"val":5},"right":null,"val":4},"next":{"$id":"7","left":{"$ref":"5"},"next":null,"right":{"$ref":"6"},"val":3},"right":{"$ref":"4"},"val":2},"next":null,"right":{"$ref":"7"},"val":1}
- *
+ * <p>
  * 解释：给定二叉树如图 A 所示，你的函数应该填充它的每个 next 指针，以指向其下一个右侧节点，如图 B 所示。
  */
 public class Connect {
     /**
+     * 迭代法
      * 层序遍历，将每层的最右边的下一个节点赋值为null；
      * 将上一个节点指向下一个节点
      *
@@ -54,6 +57,30 @@ public class Connect {
             }
         }
         return root;
+    }
+
+    /**
+     * 递归法
+     *
+     * @param root
+     * @return
+     */
+    public Node connect2(Node root) {
+        if (root == null) {
+            return null;
+        }
+        connectTwoNode(root.left, root.right);
+        return root;
+    }
+
+    private void connectTwoNode(Node node1, Node node2) {
+        if (node1 == null || node2 == null) {
+            return;
+        }
+        node1.next = node2;
+        connectTwoNode(node1.left, node1.right);
+        connectTwoNode(node2.left, node2.right);
+        connectTwoNode(node1.right, node2.left);
     }
 
     class Node {
