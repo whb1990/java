4a7c21b318c4819f7a443bedfc650985a64d7d52
wanghongbo <270028806@qq.com>
2020-11-19 08:36:57 +0800

    LeetCode-516-最长回文子序列（动态规划）

---
+++ src/main/java/com/study/leetCode/dp/LongestPalindromeSubseq.java
@@ -0,0 +1,85 @@
+package main.java.com.study.leetCode.dp;
+
+/**
+ * @author： whb
+ * @description： LeetCode-516-最长回文子序列
+ * @date： 2020-11-19 8:28
+ * 难度：中等
+ * 标签：动态规划
+ * 给定一个字符串 s ，找到其中最长的回文子序列，并返回该序列的长度。可以假设 s 的最大长度为 1000 。
+ *
+ * 示例 1:
+ * 输入:
+ *
+ * "bbbab"
+ * 输出:
+ *
+ * 4
+ * 一个可能的最长回文子序列为 "bbbb"。
+ *
+ * 示例 2:
+ * 输入:
+ *
+ * "cbbd"
+ * 输出:
+ *
+ * 2
+ * 一个可能的最长回文子序列为 "bb"。
+ *
+ * 提示：
+ * 1 <= s.length <= 1000
+ * s 只包含小写英文字母
+ */
+public class LongestPalindromeSubseq {
+    /**
+     * 动态规划
+     * 解题思路：
+     * 状态
+     * dp[i][j] 表示 s 的第 i 个字符到第 j 个字符组成的子串中，最长的回文序列长度是多少。
+     *
+     * 转移方程
+     * 如果 s 的第 i 个字符和第 j 个字符相同的话
+     *
+     * dp[i][j] = dp[i + 1][j - 1] + 2
+     *
+     * 如果 s 的第 i 个字符和第 j 个字符不同的话
+     *
+     * dp[i][j] = max(dp[i + 1][j], dp[i][j - 1])
+     *
+     * 然后注意遍历顺序，i 从最后一个字符开始往前遍历，j 从 i + 1 开始往后遍历，这样可以保证每个子问题都已经算好了。
+     *
+     * 初始化
+     * dp[i][i] = 1 单个字符的最长回文序列是 1
+     *
+     * 结果
+     * dp[0][n - 1]
+     * @param s
+     * @return
+     */
+    public static int longestPalindromeSubseq(String s) {
+        if (s.isEmpty() || s.length() <= 1) {
+            return s.length();
+        }
+        int[][] dp = new int[s.length()][s.length()];
+        for (int i = 0; i < s.length(); i++) {
+            dp[i][i] = 1;
+        }
+        // 反着遍历保证正确的状态转移
+        for (int i = s.length() - 1; i >= 0; i--) {
+            for (int j = i + 1; j < s.length(); j++) {
+                // 状态转移方程
+                if (s.charAt(i) == s.charAt(j)) {
+                    dp[i][j] = dp[i + 1][j - 1] + 2;
+                } else {
+                    dp[i][j] = Math.max(dp[i + 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        return dp[0][s.length() - 1];
+    }
+
+    public static void main(String[] args) {
+        System.out.println(longestPalindromeSubseq("bbbab"));
+        System.out.println(longestPalindromeSubseq("cbbd"));
+    }
+}
