d2935021b4c849d05f4dd6571fd36743b965718d
wanghongbo <270028806@qq.com>
2020-11-23 09:44:07 +0800

    LeetCode-5606-具有给定数值的最小字符串（贪心算法）

---
+++ src/main/java/com/study/leetCode/greedyAlgorithm/GetSmallestString.java
@@ -0,0 +1,58 @@
+package main.java.com.study.leetCode.greedyAlgorithm;
+
+/**
+ * @author： whb
+ * @description： LeetCode-5606-具有给定数值的最小字符串
+ * @date： 2020-11-23 9:35
+ * 难度：中等
+ * 标签：贪心算法
+ * 小写字符 的 数值 是它在字母表中的位置（从 1 开始），因此 a 的数值为 1 ，b 的数值为 2 ，c 的数值为 3 ，以此类推。
+ *
+ * 字符串由若干小写字符组成，字符串的数值 为各字符的数值之和。例如，字符串 "abe" 的数值等于 1 + 2 + 5 = 8 。
+ *
+ * 给你两个整数 n 和 k 。返回 长度 等于 n 且 数值 等于 k 的 字典序最小 的字符串。
+ *
+ * 注意，如果字符串 x 在字典排序中位于 y 之前，就认为 x 字典序比 y 小，有以下两种情况：
+ *
+ * x 是 y 的一个前缀；
+ * 如果 i 是 x[i] != y[i] 的第一个位置，且 x[i] 在字母表中的位置比 y[i] 靠前。
+ *
+ * 示例 1：
+ * 输入：n = 3, k = 27
+ * 输出："aay"
+ * 解释：字符串的数值为 1 + 1 + 25 = 27，它是数值满足要求且长度等于 3 字典序最小的字符串。
+ *
+ * 示例 2：
+ * 输入：n = 5, k = 73
+ * 输出："aaszz"
+ *
+ *
+ * 提示：
+ * 1 <= n <= 105
+ * n <= k <= 26 * n
+ */
+public class GetSmallestString {
+    /**
+     * 先从最大的z开始算，倒推
+     */
+    public static String getSmallestString(int n, int k) {
+        StringBuffer res = new StringBuffer();
+        for (int i = 1; i <= n; i++) {
+            int max = (n - i) * 26;
+            if (max + 1 >= k) {
+                res.append("a");
+                k--;
+            } else {
+                int tmp = k - max;
+                res.append((char) ('a' + (tmp - 1)));
+                k -= tmp;
+            }
+        }
+        return res.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getSmallestString(3, 27));
+        System.out.println(getSmallestString(5, 73));
+    }
+}
