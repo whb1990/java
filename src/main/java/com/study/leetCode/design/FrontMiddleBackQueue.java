1f008399d8fd04ea7e0730c0bddd300989ae50d7
wanghongbo <270028806@qq.com>
2020-11-30 15:58:29 +0800

    LeetCode-1670-设计前中后队列（自定义双向链表）

--- src/main/java/com/study/leetCode/design/FrontMiddleBackQueue.java
+++ src/main/java/com/study/leetCode/design/FrontMiddleBackQueue.java
@@ -140,6 +140,7 @@ public class FrontMiddleBackQueue {
     }
 
     public static void main(String[] args) {
+        System.out.println("==========双端队列实现=================");
         FrontMiddleBackQueue obj = new FrontMiddleBackQueue();
         obj.pushFront(1);
         obj.pushBack(2);
@@ -150,5 +151,179 @@ public class FrontMiddleBackQueue {
         System.out.println(obj.popMiddle());
         System.out.println(obj.popBack());
         System.out.println(obj.popFront());
+        System.out.println("****************自定义双向链表实现***************************");
+        FrontMiddleRearQueue fmrQueue = new FrontMiddleRearQueue();
+        fmrQueue.pushFront(1);
+        fmrQueue.pushBack(2);
+        fmrQueue.pushMiddle(3);
+        fmrQueue.pushMiddle(4);
+        System.out.println(fmrQueue.popFront());
+        System.out.println(fmrQueue.popMiddle());
+        System.out.println(fmrQueue.popMiddle());
+        System.out.println(fmrQueue.popBack());
+        System.out.println(fmrQueue.popFront());
+    }
+}
+
+class FrontMiddleRearQueue {
+    /**
+     * head --> 链表头部哑节点
+     * middle --> 记录链表中间节点位置
+     * tail --> 链表尾部哑节点
+     */
+    private Node head, middle, tail;
+    /**
+     * 记录链表长度
+     */
+    private int size;
+
+    public FrontMiddleRearQueue() {
+        //创建首位节点并链接在一起
+        this.head = new Node();
+        this.tail = new Node();
+        head.next = tail;
+        tail.prev = head;
+        this.size = 0;
+    }
+
+    public void pushFront(int val) {
+        Node node = new Node(val);
+        // 先该节点的next指向双向链表的第一个节点（即head.next）
+        // 再将双向链表的第一个节点（head.next）的prev指向该节点
+        node.next = head.next;
+        head.next.prev = node;
+        // 该节点与首节点连接在一起
+        node.prev = head;
+        head.next = node;
+        size++;
+        // 修改middle指针的位置
+        if (size == 1) {
+            // 说明时第一个节点，那么middle就是它
+            middle = node;
+        } else if (size % 2 == 0) {
+            // 那么middle往前移一位
+            middle = middle.prev;
+        }
+    }
+
+    public void pushMiddle(int val) {
+        if (size == 0) {
+            pushFront(val);
+            return;
+        }
+        Node node = new Node(val);
+        if (size % 2 == 0) {
+            //偶数放在middle后面
+            node.next = middle.next;
+            middle.next.prev = node;
+            node.prev = middle;
+            middle.next = node;
+        } else {
+            //奇数放在middle前面
+            node.prev = middle.prev;
+            middle.prev.next = node;
+            node.next = middle;
+            middle.prev = node;
+        }
+        size++;
+        //修改middle指针
+        if (size % 2 == 0) {
+            //偶数前移一位
+            middle = middle.prev;
+        } else {
+            //奇数后移一位
+            middle = middle.next;
+        }
+    }
+
+    public void pushBack(int val) {
+        Node node = new Node(val);
+        // 该节点的prev先指向双向链表的最后一个节点（即tail.prev）
+        // 双向链表的最后一个节点（tail.prev）的next再指向该节点
+        node.prev = tail.prev;
+        tail.prev.next = node;
+        // 该节点再与尾节点连接在一起
+        node.next = tail;
+        tail.prev = node;
+        size++;
+        //修改middle指针
+        if (size == 1) {
+            middle = node;
+        } else if (size % 2 == 1) {
+            middle = middle.next;
+        }
+    }
+
+    public int popFront() {
+        int ret = -1;
+        if (size != 0) {
+            // 当双向链表不为空时
+            // 删除双向链表的第一个节点，只要让首节点与第二个节点连接在一起即可
+            ret = head.next.val;
+            head.next.next.prev = head;
+            head.next = head.next.next;
+            size--;
+            //修改middle指针
+            if (size == 0) {
+                middle = null;
+            } else if (size % 2 == 1) {
+                middle = middle.next;
+            }
+        }
+        return ret;
+    }
+
+    public int popMiddle() {
+        int ret = -1;
+        if (size != 0) {
+            ret = middle.val;
+            middle.prev.next = middle.next;
+            middle.next.prev = middle.prev;
+            size--;
+            //修改middle指针
+            if (size % 2 == 0) {
+                middle = middle.prev;
+            } else {
+                middle = middle.next;
+            }
+        }
+        return ret;
+    }
+
+    public int popBack() {
+        int ret = -1;
+        if (size != 0) {
+            ret = tail.prev.val;
+            tail.prev.prev.next = tail;
+            tail.prev = tail.prev.prev;
+            size--;
+            //修改middle指针
+            if (size == 0) {
+                middle = null;
+            } else if (size % 2 == 0) {
+                middle = middle.prev;
+            }
+        }
+        return ret;
+    }
+
+    class Node {
+        public int val;
+        public Node prev;
+        public Node next;
+
+        public Node(int val, Node prev, Node next) {
+            this.val = val;
+            this.prev = prev;
+            this.next = next;
+        }
+
+        public Node(int val) {
+            this(val, null, null);
+        }
+
+        public Node() {
+
+        }
     }
 }
