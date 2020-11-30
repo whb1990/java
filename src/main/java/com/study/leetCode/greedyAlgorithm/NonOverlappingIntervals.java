ba3f56c43ac429547f5895e818e6b50099261f48
wanghongbo <270028806@qq.com>
2020-11-30 17:42:41 +0800

    LeetCode-435-无重叠区间（动态规划）

--- src/main/java/com/study/leetCode/greedyAlgorithm/NonOverlappingIntervals.java
+++ src/main/java/com/study/leetCode/greedyAlgorithm/NonOverlappingIntervals.java
@@ -57,10 +57,73 @@ public class NonOverlappingIntervals {
         return intervals.length - cnt;
     }
 
+    /**
+     * 动态规划，转化成最长上升子序列问题
+     * 先看下最终剩下的区间。由于剩下的区间都是不重叠的，因此剩下的相邻区间的后一个区间的开始一定是不小于前一个区间的结束的。
+     *
+     * 比如剩下的区间是[ [1,2], [2,3], [3,4] ]。就是第一个区间的 2 小于等于 第二个区间的 2，第二个区间的 3 小于等于第三个区间的 3。
+     *
+     * 不难发现如果将前面区间的结束和后面区间的开始结合起来看，其就是一个非严格递增序列。而我们的目标就是删除若干区间，从而剩下最长的非严格递增子序列。
+     *
+     * 如果对区间按照起点或者终点进行排序，那么就转化为最长递增子序列问题了。由于是一个区间，因此需要拿后面的开始和前面的结束进行比较。
+     *
+     * @param intervals
+     * @return
+     */
+    public static int eraseOverlapIntervalsDp(int[][] intervals) {
+        if (intervals == null || intervals.length < 2) {
+            return 0;
+        }
+        Arrays.sort(intervals, Comparator.comparingInt(o -> o[1]));
+        int[] dp = new int[intervals.length];
+        Arrays.fill(dp, 1);
+        int res = 1;
+        for (int i = 1; i < intervals.length; i++) {
+            for (int j = i - 1; j >= 0; j--) {
+                if (intervals[i][0] >= intervals[j][1]) {
+                    dp[i] = Math.max(dp[i], 1 + dp[j]);
+                    break;
+                }
+            }
+            dp[i] = Math.max(dp[i], dp[i - 1]);
+            res = Math.max(res, dp[i]);
+        }
+        return intervals.length - res;
+    }
+
     public static void main(String[] args) {
-        int[][] intervals = new int[][]{{1, 2}, {1, 2}, {1, 2}};
-        System.out.println(eraseOverlapIntervals(intervals));
-        int[][] intervals2 = new int[][]{{1, 2}, {2, 3}};
-        System.out.println(eraseOverlapIntervals(intervals2));
+        System.out.println("==========贪心策略============");
+        System.out.println(eraseOverlapIntervals(new int[][]{
+                {1, 2},
+                {2, 3},
+                {3, 4},
+                {1, 3}
+        }));
+        System.out.println(eraseOverlapIntervals(new int[][]{
+                {1, 2},
+                {1, 2},
+                {1, 2}
+        }));
+        System.out.println(eraseOverlapIntervals(new int[][]{
+                {1, 2},
+                {2, 3}
+        }));
+
+        System.out.println("*************动态规划*****************");
+        System.out.println(eraseOverlapIntervalsDp(new int[][]{
+                {1, 2},
+                {2, 3},
+                {3, 4},
+                {1, 3}
+        }));
+        System.out.println(eraseOverlapIntervalsDp(new int[][]{
+                {1, 2},
+                {1, 2},
+                {1, 2}
+        }));
+        System.out.println(eraseOverlapIntervalsDp(new int[][]{
+                {1, 2},
+                {2, 3}
+        }));
     }
 }
